fb1a0aeb0efc4107baf237f1bae37f724ff2d94e
Chiemerie Ezechukwu <50174206+chiemerieezechukwu@users.noreply.github.com>
2022-01-09 15:24:57 +0100

    Update. 167. Two Sum II - Input Array Is Sorted

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -10,7 +10,26 @@ Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We retu
 """
 from typing import List
 
+
 def two_sum(numbers: List[int], target: int) -> List[int]:
+    """
+    Brute force
+    Time Limit Exceeded
+
+    O(n^2) time
+    O(1) space
+    """
+    for index, val in enumerate(numbers):
+        for index2, val2 in enumerate(numbers):
+            if val + val2 == target and index != index2:
+                return [index+1, index2+1]
+
+
+def two_sum2(numbers: List[int], target: int) -> List[int]:
+    """
+    O(n) time
+    O(1) space
+    """
     hash_table = {}
     
     for index, val in enumerate(numbers):
@@ -19,3 +38,22 @@ def two_sum(numbers: List[int], target: int) -> List[int]:
             hash_table[val] = index
         else:
             return [hash_table[complement]+1, index+1]
+
+
+def twoSum3(numbers: List[int], target: int) -> List[int]:
+    """
+    The array is sorted so the operation can be done using pointers
+    O(n) time
+    O(1) space
+    """
+    start = 0
+    end = len(numbers) - 1
+    
+    while start < end:
+        sum_target = numbers[start] + numbers[end]
+        if sum_target == target:
+            return [start+1, end+1]
+        elif sum_target < target:
+            start += 1
+        else:
+            end -= 1
